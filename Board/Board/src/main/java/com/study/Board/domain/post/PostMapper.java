42148c1b294a6d0a68ec149bfbbd65971eee616b
Sang <rsw7876@naver.com>
2024-05-24 22:42:23 +0900

    sprignboot-borad

--- Board/Board/src/main/java/com/study/Board/domain/post/PostMapper.java
+++ Board/Board/src/main/java/com/study/Board/domain/post/PostMapper.java
@@ -6,14 +6,19 @@ import java.util.List;
 
 @Mapper
 public interface PostMapper {
+    // 게시글 저장
     void save(PostRequest params);
-
+    
+    // 상세 조회
     PostResponse findById(Long id);
 
+    // 게시글 수정
     void update(PostRequest params);
 
+    // 게시글 삭제
     void deleteById(Long id);
 
+    // 리스트 조회
     List<PostResponse> findAll();
 
     int count();
