e3498842b91038868d45ba12dcb702467c04e000
Sang <rsw7876@naver.com>
2024-05-27 21:36:23 +0900

    spring-CRUD

--- Board/Board/src/main/java/com/study/config/DatabaseConfig.java
+++ Board/Board/src/main/java/com/study/config/DatabaseConfig.java
@@ -37,7 +37,8 @@ public class DatabaseConfig {
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource());
-//		factoryBean.setMapperLocations(context.getResources("classpath:/mappers/**/*Mapper.xml"));
+		factoryBean.setMapperLocations(context.getResources("classpath:/mappers/**/*Mapper.xml"));
+		factoryBean.setConfiguration(mybatiConfiguration());
         return factoryBean.getObject();
     }
 
@@ -46,10 +47,10 @@ public class DatabaseConfig {
         return new SqlSessionTemplate(sqlSessionFactory());
     }
 
-//    @Bean
-//    @ConfigurationProperties(prefix = "mybatis.configuration")
-//    public org.apache.ibatis.session.Configuration mybatiConfiguration() {
-//        return new org.apache.ibatis.session.Configuration();
-//    }
+    @Bean
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration mybatiConfiguration() {
+        return new org.apache.ibatis.session.Configuration();
+    }
 
 }
