2f8a569db89c73658a42370eef7ef96610a90495
Sang <rsw7876@naver.com>
2024-05-28 22:06:38 +0900

    Post_CRUD

--- Board/Board/src/main/java/com/study/domain/post/PostService.java
+++ Board/Board/src/main/java/com/study/domain/post/PostService.java
@@ -7,11 +7,14 @@ import jakarta.transaction.Transactional;
 import java.util.List;
 
 @Service
+// 클래스 내에 final로 선언된 모든 멤버에 대한 생성자를 만들어줌.
 @RequiredArgsConstructor
 public class PostService {
 	
 	private final PostMapper postMapper;
 	
+	// 트랜잭션 처리 방법 중 하나
+	// 선언적 트랜잭션으로 불리는 기능, 메서드의 실행과 동시에 트랜잭션이 시작되고, 메서드의 정상 종료 여부에 따라 Commit 또는 Rollback 을 해준다.
 	@Transactional
 	public Long savePost(final PostRequest param) {
 		postMapper.save(param);
