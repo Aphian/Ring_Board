5409e3996a50241a87a2cfa887458dbee2852748
Sang <rsw7876@naver.com>
2024-06-03 21:15:19 +0900

    board_pagination

--- Board/Board/src/main/java/com/study/domain/post/PostController.java
+++ Board/Board/src/main/java/com/study/domain/post/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.study.common.dto.MessageDto;
 import com.study.common.dto.SearchDto;
+import com.study.common.paging.PagingResponse;
 
 import java.util.List;
 
@@ -49,8 +50,10 @@ public class PostController {
     // 게시글 리스트 페이지
     @GetMapping("/post/list.do")
     public String openPostList(@ModelAttribute("params") final SearchDto params, Model model) {
-        List<PostResponse> posts = postService.findAllPost(params);
-        model.addAttribute("posts", posts);
+//    	List<PostResponse> posts = postService.findAllPost(params);
+    	PagingResponse<PostResponse> response = postService.findAllPost(params);
+//    	model.addAttribute("posts", posts);
+        model.addAttribute("response", response);
         return "post/list";
     }
 
