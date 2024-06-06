d78d0314768d25a6cdf80e6fb093633535e3c64b
Sang <rsw7876@naver.com>
2024-06-06 16:49:35 +0900

    comment_list

--- Board/Board/src/main/java/com/study/domain/comment/CommentApiController.java
+++ Board/Board/src/main/java/com/study/domain/comment/CommentApiController.java
@@ -1,6 +1,10 @@
 package com.study.domain.comment;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,13 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class CommentController {
+public class CommentApiController {
 	
 	private final CommentService commentService;
 	
 	public CommentReponse saveCommnet(@PathVariable final Long postId, @RequestBody final CommentRequest params) {
 		Long id = commentService.saveComment(params);
 		return commentService.findCommentById(id);
+	}
+	
+	
+	@GetMapping("/post/{postId}/comments")
+	public List<CommentReponse> findAllComment(@PathVariable final Long postId) {
+		return commentService.findAllComment(postId);
 	}
 
 }
