0db2ce8b85659cb7d92b921dd0957aea34da71cf
Sang <rsw7876@naver.com>
2024-06-06 22:23:24 +0900

    comment_controller

--- Board/Board/src/main/java/com/study/domain/comment/CommentApiController.java
+++ Board/Board/src/main/java/com/study/domain/comment/CommentApiController.java
@@ -17,15 +17,30 @@ public class CommentApiController {
 	
 	private final CommentService commentService;
 	
+	@PostMapping("/posts/{postId}/comments")
 	public CommentReponse saveCommnet(@PathVariable final Long postId, @RequestBody final CommentRequest params) {
 		Long id = commentService.saveComment(params);
 		return commentService.findCommentById(id);
 	}
 	
 	
-	@GetMapping("/post/{postId}/comments")
+	@GetMapping("/posts/{postId}/comments")
 	public List<CommentReponse> findAllComment(@PathVariable final Long postId) {
 		return commentService.findAllComment(postId);
 	}
+	
+	@GetMapping("/posts/{postId}/comments/{id}")
+	public CommentReponse findCommentById(@PathVariable final Long postId, @PathVariable final Long id) {
+		return commentService.findCommentById(id);
+	}
+	
+	
+	@PostMapping("/posts/{postId}/comments/{id}")
+	public CommentReponse updateComment(@PathVariable final Long postId, @PathVariable final Long id, @RequestBody final CommentRequest params) {
+		commentService.updateComment(params);
+		return commentService.findCommentById(id);
+	}
+	
+	
 
 }
