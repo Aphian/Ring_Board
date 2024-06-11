519b58cace78109bc3148383450028eca665eeaf
Sang <rsw7876@naver.com>
2024-06-11 15:43:19 +0900

    member_controller

---
+++ Board/Board/src/main/java/com/study/domain/member/MemberController.java
@@ -0,0 +1,25 @@
+package com.study.domain.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+	
+	private final MemberService memberService;
+	
+	public String openLogin() {
+		return "member/login";
+	}
+
+}
