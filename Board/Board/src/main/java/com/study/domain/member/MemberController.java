78d705849a3d0c3ab5fa1bb178c9bb1da1ea8d46
Sang <rsw7876@naver.com>
2024-06-11 15:50:49 +0900

    member_controller

--- Board/Board/src/main/java/com/study/domain/member/MemberController.java
+++ Board/Board/src/main/java/com/study/domain/member/MemberController.java
@@ -18,8 +18,41 @@ public class MemberController {
 	
 	private final MemberService memberService;
 	
+	@GetMapping("/login.do")
 	public String openLogin() {
 		return "member/login";
 	}
+	
+	@PostMapping("/members")
+	@ResponseBody
+	public Long saveMember(@RequestBody final MemberRequest params) {
+		return memberService.saveMember(params);
+	}
+	
+	@GetMapping("/members/{loginId}")
+	@ResponseBody
+	public MemberResponse findMemberByLoginId(@PathVariable(value = "loginId", required = false) final String loginId) {
+		return memberService.findMemberByLoginId(loginId);
+	}
+	
+	@PatchMapping("/members/{id}")
+	@ResponseBody
+	public Long updateMember(@PathVariable(value = "id", required = false) final Long id, @RequestBody final MemberRequest params) {
+		return memberService.updateMember(params);
+	}
+	
+	@DeleteMapping("/members/{id}")
+	@ResponseBody
+	public Long deleteMember(final Long id) {
+		return memberService.deleteMemberById(id);
+	}
+	
+	@GetMapping("/member-count")
+	@ResponseBody
+	public int countMemberByLoginId(@RequestParam final String loginId) {
+		return memberService.countMemberByLoginId(loginId);
+	}
+	
+	
 
 }
