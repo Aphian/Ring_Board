cfa7160ea918d5e2cf28989ff402d35a79a72fe0
Sang <rsw7876@naver.com>
2024-06-12 21:58:27 +0900

    file_service

---
+++ Board/Board/src/main/java/com/study/domain/file/FileService.java
@@ -0,0 +1,33 @@
+package com.study.domain.file;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class FileService {
+	
+	private final FileMapper fileMapper;
+	
+	@Transactional
+	public void saveFiles(final Long postId, final List<FileRequest> files) {
+		
+		if (CollectionUtils.isEmpty(files)) {
+			return;
+		}
+		
+		for (FileRequest file : files) {
+			file.setPostId(postId);
+		}
+		
+		fileMapper.saveAll(files);
+		
+	}
+
+}
